c91c1b3f0f894d21fb32a10385ad1b72a809fc5d
Kevin Gutierrez <regifo010203@gmail.com>
2024-11-25 22:53:56 -0500

    Hasta ejercicio 8 clase 2

--- Modulo01/Clase02/Ejercicio07.java
+++ Modulo01/Clase02/Ejercicio07.java
@@ -1,5 +1,31 @@
 package Modulo01.Clase02;
 
+import java.util.Scanner;
+
 public class Ejercicio07 {
+
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner (System.in);    
+        
+
+        System.out.println("Digita el primer número: ");
+        double PrimerNumero = scanner.nextDouble();
+
+        System.out.println("Digita el segundo número: ");
+        double SegundoNumero = scanner.nextDouble();
+        
+
+        System.out.printf("Suma: %.2f%n", PrimerNumero + SegundoNumero);
+        System.out.printf("Resta: %.2f%n", PrimerNumero - SegundoNumero);
+        System.out.printf("Multiplicación: %.2f%n", PrimerNumero * SegundoNumero);
+        System.out.printf("División: %.2f%n", PrimerNumero/SegundoNumero);
+       
+
+        scanner.close();
+        
+
+    }
     
 }
--- Modulo01/Clase02/Ejercicio08.java
+++ Modulo01/Clase02/Ejercicio08.java
@@ -1,5 +1,22 @@
 package Modulo01.Clase02;
 
+import java.util.Scanner;
+
 public class Ejercicio08 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Ingresa la longitud del rectangulo: ");
+                double Longitud = scanner.nextDouble();
+
+            System.out.println("Ingresa el ancho del rectangulo: ");
+                double Ancho = scanner.nextDouble();
+            
+        System.out.printf("El area del rectangulo es de: %.2f cm.%n", Longitud * Ancho);
+        System.out.printf("El perímetro del rectangulo es de: %.2f cm.%n", 2 * (Longitud + Ancho));
+    
+        scanner.close();
+    }
     
 }
