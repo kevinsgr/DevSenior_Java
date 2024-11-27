40fe925b1ae3e1ac7dd28977b4b2a173b421fec2
Kevin Gutierrez <regifo010203@gmail.com>
2024-11-26 19:46:08 -0500

    Clase 02 completada

--- Modulo01/Clase02/Ejercicio09.java
+++ Modulo01/Clase02/Ejercicio09.java
@@ -1,5 +1,22 @@
 package Modulo01.Clase02;
 
+import java.util.Scanner;
+
 public class Ejercicio09 {
-    
+        public static void main(String[] args) {
+            
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digita la temperatura en grados Celsius: ");
+            Double Celsius = scanner.nextDouble() ;
+
+            System.out.println("---CONVERSION DE TEMPERATURA---");
+            System.out.printf("En grados Fahrenheit: %.2f%n", Celsius*(9/5)+32);
+            System.out.printf("En grados Kelvin: %.2f%n", Celsius+273.15);
+
+
+            scanner.close();
+        }
+
+
 }
--- Modulo01/Clase02/Ejercicio10.java
+++ Modulo01/Clase02/Ejercicio10.java
@@ -1,5 +1,28 @@
 package Modulo01.Clase02;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digita el primer numero:");
+            Double Num1 = scanner.nextDouble();
+
+            System.out.println("Digita el segundo número:");
+            Double Num2 = scanner.nextDouble();
+
+            System.out.println("Digita el tercer número:");
+            Double Num3 = scanner.nextDouble();
+
+            Double Suma = Num1 + Num2 + Num3;
+
+            Double Resultado = Suma / 3;
+
+            System.out.printf("La media aritmética de los números ingresados es %.2f", Resultado); 
+
+        scanner.close();
+    }
     
 }
